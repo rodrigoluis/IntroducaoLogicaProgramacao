f00a3eecc20e969fb5a3d6bcb8e01c7eb46cd712
rodrigoluis <rodrigoluis@gmail.com>
2023-04-26 16:52:21 -0300

    Códigos renomeados.

---
+++ chap2/cap2_ex01.cpp
@@ -0,0 +1,15 @@
+/*
+1. Faça um algoritmo para imprimir o seu nome completo, na linha seguinte sua idade e na terceira sua altura. 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float val = 2.346728;
+    cout << "João da Silva" << endl; 
+    cout << "21" << endl;
+    cout << "1.70" << endl;    
+    return 0;
+}
